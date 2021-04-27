b7a776e5270a91c580ceefe7b1c0cbd1152a65d7
Morento101 <tlcees@gmail.com>
2021-04-27 23:50:45 +0300

    Try, except were added, made functions for removing coma, outputting result and ronding to 2 decimals after dot

--- functions.py
+++ functions.py
@@ -1,18 +1,39 @@
 import math
 
 
-def permissible_residual_technical_leveling(len):
-    perm_res = 50 * math.sqrt(len)
+def coma_replace(s):
+    s = str(s)
+    if ',' in s:
+        s = s.replace(',', '.')
+    return float(s)
+
+
+def round_2(ans):
+    return round(ans, 2)
+
+
+def result(res):
+    print(f'Результат: {round_2(res)}мм')
+
+
+def permissible_residual_technical_leveling(length):
+    perm_res = 50 * math.sqrt(length)
     perm_res = math.ceil(perm_res)
-    print(f'Результат: {perm_res}мм')
+    result(perm_res)
 
 
-def permissible_residual_leveling_4class(len):
-    perm_res = 20 * math.sqrt(len)
+def permissible_residual_leveling_4class(length):
+    perm_res = 20 * math.sqrt(length)
     perm_res = math.ceil(perm_res)
-    print(f'Результат: {perm_res}мм')
+    result(perm_res)
 
 
-def intermediate_point_height(h, a):
-    inter_height = h + a
-    print(inter_height)
+def device_horizon(h, a):
+    dev_horizon = h + a
+    result(dev_horizon)
+
+
+def intermediate_point_height(h, a, c):
+    dev_horizon = h + a
+    inter_point_h = dev_horizon - c
+    result(inter_point_h)
--- main.py
+++ main.py
@@ -1,35 +1,69 @@
 import functions as fn
 
+if __name__ == '__main__':
 
-while True:
-    category = input('\nКатегорія:\n' +
-                     '1. Допустимі нев\'язки для нівелювання різними класами\n' +
-                     '2. Нівелювання траст\n'
-                     '--->')
+    while True:
+        category = input('\nКатегорія:\n' +
+                         '1. Допустимі нев\'язки для нівелювання різними класами\n' +
+                         '2. Нівелювання траси\n'
+                         '--->')
 
-    if category == '1':
-        task = input('Обчислити:\n' +
-                     '1. Допустима нев\'язка для нівелювання IV класу\n' +
-                     '2. Допустима нев\'язка для технічного нівелювання\n' +
-                     '-->')
+        if category == '1':
+            task = input('Обчислити:\n' +
+                         '1. Допустима нев\'язка для нівелювання IV класу\n' +
+                         '2. Допустима нев\'язка для технічного нівелювання\n' +
+                         '-->')
 
-        if task == '1':
-            len = float(input('Введіть відстань L в км -->'))
-            fn.permissible_residual_leveling_4class(len)
+            if task == '1':
+                try:
+                    length = input('Введіть відстань L в км -->')
+                    length = fn.coma_replace(length)
+                    fn.permissible_residual_leveling_4class(length)
 
-        if task == '2':
-            len = float(input('Введіть відстань L в км -->'))
-            fn.permissible_residual_technical_leveling(len)
+                except Exception:
+                    print('ВВЕДИ ЧИСЛО!!!')
 
-    if category == '2':
-        task = input('Обчислити:\n' +
-                     '1. Висоти проміжної точки, маючи a, c, h\n' +
-                     '2. Горизонт приладу, маючи h задньої рейки та її відліки\n' +
-                     '-->')
+            elif task == '2':
+                try:
+                    length = input('Введіть відстань L в км -->')
+                    length = fn.coma_replace(length)
+                    fn.permissible_residual_technical_leveling(length)
 
-        if task == '1':
-            h = float(input('Введіть h задньої точки -->'))
-            a = float(input('Введіть с задньої точки -->'))
-            fn.intermediate_point_height(h, a)
+                except Exception:
+                    print('ВВЕДИ ЧИСЛО!!!')
 
+            else:
+                print('ВВЕДИ ЧИСЛО!!!')
 
+        elif category == '2':
+            task = input('Обчислити:\n' +
+                         '1. Висоти проміжної точки, маючи a, c, h\n' +
+                         '2. Горизонт приладу, маючи h задньої рейки та її відліки\n' +
+                         '-->')
+
+            if task == '1':
+                try:
+                    h = input('Введіть h задньої точки -->')
+                    a = input('Введіть a задньої точки -->')
+                    c = input('Введіть c задньої точки -->')
+                    h, a, c = fn.coma_replace(h), fn.coma_replace(a), fn.coma_replace(c)
+                    fn.intermediate_point_height(h, a, c)
+
+                except Exception:
+                    print('ВВЕДИ ЧИСЛО!!!')
+
+            elif task == '2':
+                try:
+                    h = input('Введіть h задньої точки -->')
+                    a = input('Введіть a задньої точки -->')
+                    h, a = fn.coma_replace(h), fn.coma_replace(a)
+                    fn.device_horizon(h, a)
+
+                except Exception:
+                    print('ВВЕДИ ЧИСЛО!!!')
+
+            else:
+                print('ВВЕДИ ЧИСЛО!!!')
+
+        else:
+            print('ВВЕДИ ЧИСЛО!!!')
